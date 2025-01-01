11c6c9c2a50dba960e6295896c46700811054ad7
Abu Bakar <abubakarbinzohaib@gmail.com>
2025-01-01 05:18:18 -0800

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { motion } from 'framer-motion'
-import { ArrowRight, GithubIcon, Linkedin, FacebookIcon, Github, LinkedinIcon } from 'lucide-react'
+import { ArrowRight, GithubIcon, FacebookIcon, LinkedinIcon } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 
 const Hero= () => {
