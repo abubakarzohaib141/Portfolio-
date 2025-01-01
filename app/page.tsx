394908195d1cdebcfcaa35b69a8d8eea29cf4c58
Abu Bakar <abubakarbinzohaib@gmail.com>
2025-01-01 05:12:07 -0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Hero from "@/components/Hero";
 import Projects from "@/components/Projects";
 import SkillsSection from "@/components/Skills";
-import Image from "next/image";
 import Footer from "@/components/Footer";
 export default function Home() {
   return (
